41dae5234252e6729c3b6206c5dd11b1e2332cc0
sldless <67599596+sldless@users.noreply.github.com>
2022-02-18 17:00:02 -0500

    Movements side to side

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,19 @@
 extends KinematicBody2D
 
 func _ready():
-	draw_circle(Vector2(512, 300), 10, Color.green)
-	get_node("CollisionShape2D").position.x = Vector2(512,300)
+	_draw()
+	
+func _draw():
+	draw_circle(Vector2(512, 300), 20, Color.green)
+	get_node("TestBase").position = Vector2(510, 200)
+
+var _velocity := Vector2.ZERO
+
+func _physics_process(_delta) -> void:
+	var _hori_directions = (
+		Input.get_action_strength("move_right")
+		- Input.get_action_strength('move_left')
+	)
+	_velocity.x = _hori_directions *  10
+	_velocity.y += 1400.0 * _delta
+	_velocity = move_and_slide(_velocity, Vector2.UP)
