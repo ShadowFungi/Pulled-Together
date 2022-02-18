7f4067c954116ba1e271a52c78f872c6cf01fca7
sldless <67599596+sldless@users.noreply.github.com>
2022-02-18 18:14:16 -0500

    commit

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,7 +7,7 @@ func _physics_process(_delta) -> void:
 	var _hori_directions = (
 		Input.get_action_strength("move_right")
 		- Input.get_action_strength('move_left')
-	)
-	_velocity.x = _hori_directions *  10
-	_velocity.y += 1400.0 * _delta
+		)
+	_velocity.x =  _hori_directions *  10
+	_velocity.y += 1800.0 * _delta
 	_velocity = move_and_slide(_velocity, Vector2.UP)
