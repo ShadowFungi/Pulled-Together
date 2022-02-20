6fdee4c8aac65e7ce34896c541ae705272ba8926
ShadowFungi <shadowfungi.meddr@aleeas.com>
2022-02-19 21:13:48 -0700

    ay look grapple works
    but not upwards

--- Scripts/Player/Grapple/Arrow.gd
+++ Scripts/Player/Grapple/Arrow.gd
@@ -12,10 +12,21 @@ func contact():
 	mouse.set_global_position(get_global_mouse_position())
 	ray.look_at(get_global_mouse_position())
 	posit = [position, ray.get_cast_to()]
-	if mouse.position.x > self.position.x:
-		ray.cast_to.x = -mouse.position.x
-	if mouse.position.x >= -250:
-		ray.cast_to.x = -mouse.position.x
+	if mouse.position.y != self.position.y:
+		if mouse.position.x < 250:
+			if mouse.position.x > self.position.x:
+				ray.cast_to.x = mouse.position.x
+			elif mouse.position.x < self.position.x:
+				ray.cast_to.x = -mouse.position.x
+				
+	elif mouse.position.x != self.position.x:
+		if mouse.position.y < 250:
+			if mouse.position.y > self.position.y:
+				ray.cast_to.x = mouse.position.y
+			elif mouse.position.y < self.position.y:
+				ray.cast_to.x = -mouse.position.y
+				
 	else:
 		ray.cast_to.x = 250
+		
 	tail.set_points(PoolVector2Array(posit))
